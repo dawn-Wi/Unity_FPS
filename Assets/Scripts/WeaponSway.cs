6b287bf8fe1ff3dd4aeef399881011b65b87495e
dawn <wi825@naver.com>
2022-11-16 18:17:42 +0900

    Sway

--- Assets/Scripts/WeaponSway.cs
+++ Assets/Scripts/WeaponSway.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class WeaponSway : MonoBehaviour
 {
+    private Vector3 _originPos;
+
+    private Vector3 _currentPos;
+
+    [SerializeField] 
+    private Vector3 _limitPos;
+
+    [SerializeField]
+    private Vector3 _fineSigntLimitPos;
+
+    [SerializeField] 
+    private Vector3 _smoothSway;
+
+    [SerializeField] 
+    private GunController _theGunController;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        _originPos = this.transform.localPosition;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        TrySway();
+    }
+
+    private void TrySway()
+    {
+        if (Input.GetAxisRaw("Mouse X") != 0|| Input.GetAxisRaw("Mouse Y")!= 0)
+        {
+            Swaying();
+        }
+        else
+        {
+            BackToOriginPos();
+        }
+    }
+
+    private void Swaying()
+    {
+        float _moveX = Input.GetAxisRaw("Mouse X");
+        float _moveY = Input.GetAxisRaw("Mouse Y");
+
+        if (_theGunController._isFineSightMode)
+        {
+            _currentPos.Set(Mathf.Clamp(Mathf.Lerp(_currentPos.x, -_moveX, _smoothSway.x), -_limitPos.x, _limitPos.x), 
+                Mathf.Clamp(Mathf.Lerp(_currentPos.y, -_moveX, _smoothSway.x), -_limitPos.y, _limitPos.y),
+                _originPos.z);
+        }
+        else
+        {
+            _currentPos.Set(Mathf.Clamp(Mathf.Lerp(_currentPos.x, -_moveX, _smoothSway.y), -_fineSigntLimitPos.x, _fineSigntLimitPos.x), 
+                Mathf.Clamp(Mathf.Lerp(_currentPos.y, -_moveX, _smoothSway.y), -_fineSigntLimitPos.y, _fineSigntLimitPos.y),
+                _originPos.z);
+        }
+        transform.localPosition = _currentPos;
+    }
+
+    private void BackToOriginPos()
+    {
+        _currentPos = Vector3.Lerp(_currentPos, _originPos, _smoothSway.x);
+        transform.localPosition = _currentPos;
     }
 }
